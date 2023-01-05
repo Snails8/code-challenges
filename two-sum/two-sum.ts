1492b08ccf904b8c997c765ed17c19092e29d67a
Kaito Suzuki <Snails8d@gmail.com>
2023-01-05 21:25:43 +0900

    Create two-sum.ts

---
+++ two-sum/two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    let result: number[] = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                result.push(i, j);
+                return result;
+            }
+        }
+    }
+};
