9f5eae5b3d30444556af89c7657d66cc6c55af6f
Kaito Suzuki <Snails8d@gmail.com>
2023-01-09 13:17:26 +0900

    Create longest-common-prefix.ts

---
+++ longest common prefix/longest-common-prefix.ts
@@ -0,0 +1,15 @@
+function longestCommonPrefix(strs: string[]): string {
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+        }
+        
+        if (prefix === '') {
+            return prefix;
+        }
+    }
+
+    return prefix;
+};
