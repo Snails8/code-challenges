50ce1965c047d83b3cbec213393cf1414b22c507
Kaito Suzuki <Snails8d@gmail.com>
2023-06-08 22:00:04 +0900

    Create main.py

---
+++ atcoder/abc/122-b/main.py
@@ -0,0 +1,21 @@
+"""
+英大文字からなる文字列 S が与えられます。
+S の部分文字列であるような最も長い ACGT 文字列 の長さを求めてください。
+
+ここで、ACGT 文字列とは A, C, G, T 以外の文字を含まない文字列です。
+"""
+
+S = input()
+
+answer = 0
+count = 0
+
+for i in range(len(S)):
+  if S[i] in "ACGT":
+    count += 1
+    answer = max(answer, count)
+  else:
+     count = 0
+
+print(answer)
+
